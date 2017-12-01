10ac0d82e156110ee482d6d24cf10e9a1fcc33e5
Jim Whurr <jim.whurr@outlook.com>
2017-12-01 20:23:20 +0000

    Configured for heroku

--- server/config/config.js
+++ server/config/config.js
@@ -9,3 +9,7 @@ else if (env === 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://192.168.0.126:27017/TodoAppTest';
 }
+else {
+    // db on mlab, production code for heroku
+    process.env.MONGODB_URI = 'mongodb://jimwhurr;5MarshRise@ds127506.mlab.com:27506/todoapp';    
+}
